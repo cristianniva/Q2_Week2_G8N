58619347b406a4c11f0007a04d1eae4a7a392246
Mr.Cristian <cristiana@nivais.com>
2021-10-18 09:37:09 +0700

    Class 1: creating variables in Javascript

---
+++ clase1.js
@@ -0,0 +1,9 @@
+console.log('Hi Im Mr. Cristian from your ILT Class')
+
+var name = 'Cristian', surname = 'Angulo' // how we declare/create variables
+var age = 30
+
+age = '30 years'
+
+console.log('Hello ' + name + ' ' + surname)
+console.log('I am ' + age)
