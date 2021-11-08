3ad6e26148a8da6293507bd8f78edaa45c02f82e
AyaMiyamoto <1848@niva.ac.th>
2021-11-08 11:28:53 +0700

    this is my commit

--- class1.js
+++ class1.js
@@ -1,9 +1,9 @@
 console.log('Hi Im Mr. Cristian from your ILT Class')
 
-var name = 'Cristian', surname = 'Angulo' // how we declare/create variables
-var age = 30
+var myname = 'Aya', surname = 'Miyamoto' // how we declare/create variables
+var age = 14
 
-age = '30 years'
+age = '14 years'
 
-console.log('Hello ' + name + ' ' + surname)
+console.log('Hello ' + myname + ' ' + surname)
 console.log('I am ' + age)
